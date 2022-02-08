e9e4877ce22f025d22232673c609310143ea5df4
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-02-08 12:56:15 +0100

    first

---
+++ src/Console.elm
@@ -0,0 +1,66 @@
+module Console exposing (..)
+
+{-| <https://nodejs.org/api/console.html>
+-}
+
+import JavaScript
+import Json.Decode
+import Json.Encode
+import Task
+
+
+log : String -> Task.Task JavaScript.Error String
+log a =
+    JavaScript.run "console.log(a)"
+        (Json.Encode.string a)
+        (Json.Decode.succeed a)
+
+
+time : String -> Task.Task JavaScript.Error ()
+time a =
+    JavaScript.run "console.time(a)"
+        (Json.Encode.string a)
+        (Json.Decode.succeed ())
+
+
+timeEnd : String -> Task.Task JavaScript.Error ()
+timeEnd a =
+    JavaScript.run "console.timeEnd(a)"
+        (Json.Encode.string a)
+        (Json.Decode.succeed ())
+
+
+timeTask : (JavaScript.Error -> x) -> String -> Task.Task x a -> Task.Task x a
+timeTask toError label a =
+    time label
+        |> Task.mapError toError
+        |> Task.andThen
+            (\_ ->
+                a
+                    |> taskAndThenWithResult
+                        (\v ->
+                            timeEnd label
+                                |> Task.mapError toError
+                                |> Task.andThen
+                                    (\_ ->
+                                        case v of
+                                            Ok v2 ->
+                                                Task.succeed v2
+
+                                            Err v2 ->
+                                                Task.fail v2
+                                    )
+                        )
+            )
+
+
+
+--
+
+
+taskAndThenWithResult : (Result x a -> Task.Task y b) -> Task.Task x a -> Task.Task y b
+taskAndThenWithResult toTask a =
+    a
+        |> Task.map Ok
+        |> Task.onError (Err >> Task.succeed)
+        |> Task.andThen toTask
---
+++ src/FileSystem.elm
@@ -0,0 +1,49 @@
+module FileSystem exposing (..)
+
+{-| <https://nodejs.org/api/fs.html>
+-}
+
+import JavaScript
+import Json.Decode
+import Json.Encode
+import Task
+
+
+type Path
+    = Path String
+
+
+
+--
+
+
+read : Path -> Task.Task JavaScript.Error String
+read (Path a) =
+    JavaScript.run
+        "require('fs/promises').readFile(a, 'utf-8')"
+        (Json.Encode.string a)
+        Json.Decode.string
+
+
+write : Path -> String -> Task.Task JavaScript.Error ()
+write (Path a) data =
+    JavaScript.run
+        "require('fs/promises').writeFile(a.path, a.data)"
+        (Json.Encode.object
+            [ ( "path", Json.Encode.string a )
+            , ( "data", Json.Encode.string data )
+            ]
+        )
+        (Json.Decode.succeed ())
+
+
+append : Path -> String -> Task.Task JavaScript.Error ()
+append (Path a) data =
+    JavaScript.run
+        "require('fs/promises').appendFile(a.path, a.data)"
+        (Json.Encode.object
+            [ ( "path", Json.Encode.string a )
+            , ( "data", Json.Encode.string data )
+            ]
+        )
+        (Json.Decode.succeed ())
