db335a9d9e16610ab3d30c0078374fbafff1637f
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-08-24 17:28:34 +0200

    change return type

--- src/Console.elm
+++ src/Console.elm
@@ -9,32 +9,32 @@ import Json.Encode
 import Task
 
 
-log : String -> Task.Task JavaScript.Error String
+log : String -> Task.Task JavaScript.Error ()
 log a =
     JavaScript.run "console.log(a)"
         (Json.Encode.string a)
-        (Json.Decode.succeed a)
+        (Json.Decode.succeed ())
 
 
-logInfo : String -> Task.Task JavaScript.Error String
+logInfo : String -> Task.Task JavaScript.Error ()
 logInfo a =
     JavaScript.run "console.info(a)"
         (Json.Encode.string a)
-        (Json.Decode.succeed a)
+        (Json.Decode.succeed ())
 
 
-logWarning : String -> Task.Task JavaScript.Error String
+logWarning : String -> Task.Task JavaScript.Error ()
 logWarning a =
     JavaScript.run "console.warn(a)"
         (Json.Encode.string a)
-        (Json.Decode.succeed a)
+        (Json.Decode.succeed ())
 
 
-logError : String -> Task.Task JavaScript.Error String
+logError : String -> Task.Task JavaScript.Error ()
 logError a =
     JavaScript.run "console.error(a)"
         (Json.Encode.string a)
-        (Json.Decode.succeed a)
+        (Json.Decode.succeed ())
 
 
 time : String -> Task.Task JavaScript.Error ()
